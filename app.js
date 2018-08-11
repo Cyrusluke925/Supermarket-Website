6dd1e13ebda80d3e23ef36c425f51f1af2fcd709
Luke <luke@Lukes-MacBook-Pro.local>
2018-08-11 14:48:51 -0700

    started manipulating options page using DOM

---
+++ app.js
@@ -0,0 +1,133 @@
+var alcohol = [
+    {
+        picture: './pictures/absolut.jpg',
+        name: 'Vodka'
+    },
+
+    {
+        picture: './pictures/gin.jpg',
+        name: 'Gin'
+    },
+
+    {
+        picture: './pictures/rum.jpg',
+        name: 'Rum'
+    },
+
+    {
+        picture: './pictures/tequila.jpg',
+        name: 'Tequila'
+    },
+
+]
+
+
+var candy = [
+    
+    {
+    picture: './pictures/kitkat.jpg',
+    name: 'Kit Kat'
+    },
+
+    {
+        picture: './pictures/starbursts.jpeg',
+        name: 'Starbursts'
+    },
+
+    {
+        picture: './pictures/skittles.jpg',
+        name: 'Skittles'
+    },
+    {
+        picture: './pictures/snickers.jpeg',
+        name: 'Snickers'
+    }
+]
+
+// page elements
+var divTag = document.createElement('div');
+var pTag = document.createElement('p');
+var optionsPage = document.getElementById('options')
+
+//link tabs 
+
+var alcoholTab = document.getElementById('Alcohol');
+var candyTab = document.getElementById('Candy');
+var cerealTab = document.getElementById('Cereal');
+
+function alcoholPage() {
+
+    console.log(alcohol)
+        optionsPage.removeChild('divTag')
+    for (let i = 0; i < alcohol.length; i += 1) {
+    
+        var currentDrink = alcohol[i];
+        var liquid = document.createElement('img')
+        liquid.setAttribute('src', currentDrink.picture);
+        divTag.appendChild(liquid);
+        optionsPage.appendChild(divTag);
+
+    }
+}
+
+function candyPage() {
+    console.log(candy)
+    for(let i = 0;i < candy.length; i += 1) {
+    let currentItem = candy[i];
+    let item = document.createElement('img');
+    item.setAttribute('src', currentItem.picture);
+    divTag.appendChild(item);
+    optionsPage.appendChild(divTag);
+
+
+    }
+}
+
+
+alcoholTab.addEventListener('click', alcoholPage);
+candyTab.addEventListener('click', candyPage);
+
+
+
+
+
+// var getAlcohol = document.getElementById('candy')
+
+// function changeColor() {
+//     if (this.style.color === 'black') {
+//         this.style.color = 'white';
+//     } else {
+//         this.style.color = 'black';
+//     }
+// }
+
+
+
+// var alcoholSelection = document.createElement('div');
+// var optionSection = document.getElementById('options');
+// var selection = optionSection.appendChild(alcoholSelection);
+
+
+//Alcohol BUTTON
+
+// getAlcohol.addEventListener('click', showAlcohol);
+
+
+// function showAlcohol() {
+// selection.innerHTML = `<a href="#"><img src="./pictures/absolut.jpg" alt="vodka" width=150px></a><p>Vodka</p>
+
+// <a href="#"><img src="./pictures/gin.jpg" width=180px></a><p>gin</p>
+
+// <a href="#"><img src="./pictures/rum.jpg" width= 180px></a><p>rum</p>
+
+// <a href="#"><img src="./pictures/tequila.jpg" width= 150px></a><p>Tequila</p>`
+
+
+// };
+
+
+
+
+
+
+
