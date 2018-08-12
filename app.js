46e3e9f3c03764f51ae4b0791beccd49dc3fe02d
Luke <luke@Lukes-MacBook-Pro.local>
2018-08-11 21:28:28 -0700

    optimized and consolidated Javascript page

--- app.js
+++ app.js
@@ -1,4 +1,25 @@
-var alcohol = [
+var sections = [
+
+[
+        {
+            picture: './pictures/orbit.jpg',
+            name: 'Orbit'
+        },
+        {
+            picture: './pictures/trident.jpeg',
+            name: 'Trident'
+        },
+        {
+            picture: './pictures/stride.jpg',
+            name: 'Stride'
+        },
+        {
+            picture: './pictures/extra.jpg',
+            name: 'Extra'
+        }
+    ],
+
+[
     {
         picture: './pictures/absolut.jpg',
         name: 'Vodka'
@@ -19,10 +40,9 @@ var alcohol = [
         name: 'Tequila'
     },
 
-]
+],
 
-
-var candy = [
+[
     
     {
     picture: './pictures/kitkat.jpeg',
@@ -44,137 +64,60 @@ var candy = [
     }
 ]
 
-var gum = [
-    {
-        picture: './pictures/orbit.jpg',
-        name: 'Orbit'
-    },
-    {
-        picture: './pictures/trident.jpeg',
-        name: 'Trident'
-    },
-    {
-        picture: './pictures/stride.jpg',
-        name: 'Stride'
-    },
-    {
-        picture: './pictures/extra.jpg',
-        name: 'Extra'
-    }
+
 ]
 
-// page elements
-var divTag = document.createElement('div')
-var checkOut = document.getElementById('checkout');
-
-
-// var pTag = document.createElement('p');
-var optionsPage = document.getElementById('options')
-var images = document.getElementsByTagName('img')
-
-//link tabs 
+//sections/Categories
 
 var alcoholTab = document.getElementById('Alcohol');
 var candyTab = document.getElementById('Candy');
 var gumTab = document.getElementById('Gum');
-var mainTag = document.getElementsByTagName('main');
+var tabs = [gumTab, alcoholTab, candyTab];
+var optionsPage = document.getElementById('options');
+var checkOut = document.getElementById('checkout');
 
-function alcoholPage() {
-if (images.length !== 0 ) {
-    divTag.innerHTML = "";
-    } 
-    for (let i = 0; i < alcohol.length; i += 1) {
-        let currentItem = alcohol[i];
-        let articleTag = document.createElement('article');
-        let item = document.createElement('img');
-        let pTag = document.createElement('p');
-        item.setAttribute('src', currentItem.picture);
-        articleTag.appendChild(item);
-        pTag.innerText = currentItem.name;
-        articleTag.appendChild(pTag);
-        divTag.appendChild(articleTag);
-        optionsPage.appendChild(divTag);
 
-        articleTag.addEventListener('click', function () {
-            var checkOutTag = document.createElement('h3');
-            checkOutTag.innerText = currentItem.name;
-            checkOut.appendChild(checkOutTag)
-            
-        });
+
+//TAGS
+var images = document.getElementsByTagName('img');
+var divTag = document.createElement('div');
+
+
+//Body of Code for left side of Screen. Adding elements to page using DOM
+for (let i = 0; i < tabs.length; i += 1) {
+    var tab = tabs[i];
+    console.log(tab);
     
-    }
-}
-
-function candyPage() {
-    if (images.length !== 0 ) {
-        divTag.innerHTML = "";
-        } 
-    for(let i = 0;i < candy.length; i += 1) {
-    let currentItem = candy[i];
-    let articleTag = document.createElement('article');
-    let item = document.createElement('img');
-    let pTag = document.createElement('p');
-    item.setAttribute('src', currentItem.picture);
-    articleTag.appendChild(item);
-    pTag.innerText = currentItem.name;
-    articleTag.appendChild(pTag);
-    divTag.appendChild(articleTag);
-    optionsPage.appendChild(divTag);
-
-    articleTag.addEventListener('click', function () {
-        var checkOutTag = document.createElement('h3');
-        checkOutTag.innerText = currentItem.name;
-        checkOut.appendChild(checkOutTag)
+    tab.addEventListener('click', function() {
+        var currentSection = sections[i];
         
-    });
-    }
-}
+        //checks to see if imgs are already on the page and clears them if there is
+        if (images.length !== 0) {
+            divTag.innerHTML = "";
+        }
+
+        for (let k = 0; k < currentSection.length; k += 1) {
+            var currentItem = currentSection[k];
+            console.log(currentItem);
+            let articleTag = document.createElement('article');
+            let item = document.createElement('img');
+            let pTag = document.createElement('p');
+            item.setAttribute('src', currentItem.picture);
+            articleTag.appendChild(item);
+            pTag.innerText = currentItem.name;
+            articleTag.appendChild(pTag);
+            divTag.appendChild(articleTag);
+            optionsPage.appendChild(divTag);
 
 
-
-function gumPage() {
-    if (images.length !== 0 ) {
-        divTag.innerHTML = "";
-        } 
-    for(let i = 0;i < gum.length; i += 1) {
-    let currentItem = gum[i];
-    let articleTag = document.createElement('article');
-    let item = document.createElement('img');
-    let pTag = document.createElement('p');
-    item.setAttribute('src', currentItem.picture);
-    articleTag.appendChild(item);
-    pTag.innerText = currentItem.name;
-    articleTag.appendChild(pTag);
-    divTag.appendChild(articleTag);
-    optionsPage.appendChild(divTag);
-    
-    articleTag.addEventListener('click', function () {
-        var checkOutTag = document.createElement('h3');
-        checkOutTag.innerText = currentItem.name;
-        checkOut.appendChild(checkOutTag)
-        
+            //ChECKOUT section of page.
+            articleTag.addEventListener('click', function() {
+                var checkOutTag = document.createElement('h3');
+                checkOutTag.innerText = currentItem.name;
+                checkOut.appendChild(checkOutTag);
+            })
+        }
     });
     
-    }
-}
 
-
-
-
-
-alcoholTab.addEventListener('click', alcoholPage);
-candyTab.addEventListener('click', candyPage);
-gumTab.addEventListener('click', gumPage);
-
-
-
-
-
-
-
-
-
-
-
-
-
+};
